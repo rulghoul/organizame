17cf2f6931311ad1dc6d7cae9617427a72c43832
Raul Perez <ghoulrul@gmail.com>
2024-09-17 09:17:50 -0600

    Se modifica el nombre del archivo bouncer_api_key agregandoo la extencion txt

--- traefik/CrowdSec/entry.sh
+++ traefik/CrowdSec/entry.sh
@@ -2,7 +2,7 @@
 
 # Lee la clave API del archivo montado y la exporta como variable de entorno
 if [ -f /etc/crowdsec/bouncer_api_key ]; then
-  export CROWDSEC_BOUNCER_API_KEY=$(cat /etc/crowdsec/bouncer_api_key)
+  export CROWDSEC_BOUNCER_API_KEY=$(cat /etc/crowdsec/bouncer_api_key.txt)
 else
   echo "Error: No se encontró el archivo de la API key en /etc/crowdsec/bouncer_api_key"
   exit 1
--- traefik/CrowdSec/init.sh
+++ traefik/CrowdSec/init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 if [ ! -f /etc/crowdsec/bouncer_api_key ]; then
-  cscli bouncers add bouncer-traefik > /etc/crowdsec/bouncer_api_key
+  cscli bouncers add bouncer-traefik > /etc/crowdsec/bouncer_api_key.txt
 fi
